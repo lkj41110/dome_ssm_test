68492b7f3c6da2da3936ee94109a96efe38fe5d4
lkj41110 <807637152@qq.com>
2018-01-08 14:46:29 +0800

    add: 数据库测试

--- dome_admin/page/src/services/functions.js
+++ dome_admin/page/src/services/functions.js
@@ -1,70 +1,83 @@
-import React,{ Component } from 'react';
-
-function add0(m){
-    return m<10?'0'+m:m;
+//货币的形式显示
+export function showPayment(mon) {
+  if (isNaN(mon) || mon == 0) {
+    return '-';
+  }
+  let money = mon / 100;
+  money = String(money);
+  let left = money.split('.')[0], right = money.split('.')[1];
+  right = right ? (right.length >= 2 ? '.' + right.substr(0, 2) : '.' + right + '0') : '.00';
+  let temp = left.split('').reverse().join('').match(/(\d{1,3})/g);
+  return (Number(money) < 0 ? "-" : "") + temp.join(',').split('').reverse().join('') + right;
 }
 
-export function formatDate(shijianchuo){
-    var time = new Date(shijianchuo);
-    var y = time.getFullYear();
-    var m = time.getMonth()+1;
-    var d = time.getDate();
-    var h = time.getHours();
-    var mm = time.getMinutes();
-    var s = time.getSeconds();
-    return y+'-'+add0(m)+'-'+add0(d)+' '+add0(h)+':'+add0(mm)+':'+add0(s);
+//分转换为元，若为空则显示defaultStr
+export function fenToYuan(money, defaultStr) {
+  if (isNaN(money)) {
+    return defaultStr;
+  }
+  return money / 100;
+}
+//百分比显示
+export function Percentage(value) {
+  if (isNaN(value)) {
+    return '-';
+  }
+  if (value == 0) {
+    return "-";
+  }
+  return parseFloat(value) * 100 + "%";
+}
+//百分比显示
+export function Percentage2(value, defaultStr) {
+  if (isNaN(value)) {
+    return defaultStr;
+  }
+  if (value == 0) {
+    return defaultStr;
+  }
+  return (value * 100).toFixed(2) + "%";
+}
+//时间格式转换
+export function getYMD(time) {
+  if (!time) {
+    return "--";
+  }
+  time = time.replace(new RegExp("-", "gm"), "/"); //为了适配所有的浏览器
+  var date = new Date(time);
+  var y = date.getFullYear();
+  var m = date.getMonth() + 1;
+  var d = date.getDate();
+  return y + '-' + add0(m) + '-' + add0(d);
+}
+//解析Josn
+export function parseJSON(jsonStr){
+  let jsonObj = null;
+  try {
+    jsonObj = JSON.parse(jsonStr);
+  }catch(e){
+    console.log('解析json格式失败，解析内容：',jsonStr);
+  }
+  return jsonObj;
 }
 
-export function getDay(shijianchuo){
-    var time = new Date(shijianchuo);
-    var d = time.getDate();
-    return d;
+function add0(m) {
+  return m < 10 ? '0' + m : m;
 }
 
-export function getYMD(shijianchuo){
-    var time = new Date(shijianchuo);
-    var y = time.getFullYear();
-    var m = time.getMonth()+1;
-    var d = time.getDate();
-    var h = time.getHours();
-    var mm = time.getMinutes();
-    var s = time.getSeconds();
-    return y+'-'+add0(m)+'-'+add0(d);
+//删除左右两端的空格
+export function trim(str) {
+  return str.replace(/(^\s*)|(\s*$)/g, "");
 }
 
-export function formatDateZero(shijianchuo){
-    var time = new Date(shijianchuo);
-    var y = time.getFullYear();
-    var m = time.getMonth()+1;
-    var d = time.getDate();
-    var h = time.getHours();
-    var mm = time.getMinutes();
-    var s = time.getSeconds();
-    return y+'-'+add0(m)+'-'+add0(d)+' 00:00:00';
+//判断是否是电话号码
+export function isMobile(str) {
+  let mobile = /^[1][3,4,5,7,8][0-9]{9}$/;
+  return (mobile.test(str));
 }
 
-export function formatDateEnd(shijianchuo){
-    var time = new Date(shijianchuo);
-    var y = time.getFullYear();
-    var m = time.getMonth()+1;
-    var d = time.getDate();
-    var h = time.getHours();
-    var mm = time.getMinutes();
-    var s = time.getSeconds();
-    return y+'-'+add0(m)+'-'+add0(d)+' 23:59:59';
-}
 
-export function fenToYuan(fen){
-    if(isNaN(fen)){
-        return '-';
-    }
-    return fen / 100;
-}
 
-export function fenToWan(fen){
-    if(isNaN(fen)){
-        return '-';
-    }
-    return fen / 100 / 10000;
-}
+
+
 
---
+++ dome_admin/src/test/java/springTest/ResourceTest.java
@@ -0,0 +1,86 @@
+package springTest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.FileSystemResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * spring 资源测试类
+ *
+ * @author luokai
+ * @date 2017/11/30
+ */
+@Slf4j
+public class ResourceTest {
+
+    @Test
+    public void testDefaultResourceLoader() {
+        /**
+         * DefaultResourceLoader
+         * getResource来产生产生实例化资源
+         * getResourceByPath为模板方法，具体的实现有子类实现
+         * 三种判断逻辑，可能会产生没有不存在的资源。
+         */
+        ResourceLoader resourceLoader = new DefaultResourceLoader();
+        Resource fakeFileResource = resourceLoader.getResource("/Users/lkj41110/Downloads/programmer/dome_ssm_test/dome_admin/pom.xml");
+        assertTrue(fakeFileResource instanceof ClassPathResource);
+        //产生的实际上并不存在的资源
+        assertFalse(fakeFileResource.exists());
+        Resource urlResource1 = resourceLoader.getResource("file:/Users/lkj41110/Downloads/programmer/dome_ssm_test/dome_admin/pom.xml");
+        assertTrue(urlResource1 instanceof UrlResource);
+        Resource urlResource2 = resourceLoader.getResource("http://www.spring21.cn");
+        assertTrue(urlResource2 instanceof UrlResource);
+    }
+
+    @Test
+    public void testResourceTypesWithFileSystemResourceLoader() {
+        ResourceLoader resourceLoader = new FileSystemResourceLoader();
+        Resource fileResource = resourceLoader.getResource("/Users/lkj41110/Downloads/programmer/dome_ssm_test/README");
+        assertTrue(fileResource instanceof FileSystemResource);
+        Resource urlResource = resourceLoader.getResource("file:/Users/lkj41110/Downloads/programmer/dome_ssm_test/README");
+        assertTrue(urlResource instanceof UrlResource);
+        assertTrue(urlResource.exists());
+    }
+
+    /**
+     * ResourcePatternResolver 资源加载器可以加载多个配置文件
+     * 不设置默认的加载器，会使用DefaultResourceLoader来加载配置文件
+     * 不可在创建的时候指定构造器 PathMatchingResourcePatternResolver(ResourceLoader resourceLoader)
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testPathMatchingResourcePatternResolver() throws IOException {
+        ResourcePatternResolver resourceResolver = new PathMatchingResourcePatternResolver();
+        Resource fileResource = resourceResolver.getResource("classpath:jdbc.properties");
+        assertTrue(fileResource instanceof ClassPathResource);
+        assertTrue(fileResource.exists());
+
+        Resource[] fileResources = resourceResolver.getResources("classpath:*.properties");
+        assertTrue(fileResources.length > 0);
+    }
+
+    @Test
+    public void testApplicationContext() throws IOException {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("jdbc.properties");
+        ApplicationContext ctx2 = new FileSystemXmlApplicationContext("conf/appContext.xml");
+    }
+
+}

3ab3c70035e23154507925c9f31e883c4abc2428
lkj41110 <807637152@qq.com>
2019-12-15 19:34:36 +0800

    MyBeanPostProcessor Test

---
+++ dome_admin/src/test/java/springTest/factory/InitMethodBeanFactory.java
@@ -0,0 +1,51 @@
+package springTest.factory;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.core.io.ClassPathResource;
+
+
+/**
+ * @author luokai
+ * @date 2017/11/30
+ */
+@Slf4j
+public class InitMethodBeanFactory {
+
+    @Test
+    public void getBeanTest() {
+        ClassPathResource resource = new ClassPathResource("spring/spring-initmethod.xml");
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(factory);
+        beanDefinitionReader.loadBeanDefinitions(resource);
+
+        InitMethodBean bean = (InitMethodBean) factory.getBean("initMethodBean");
+    }
+
+}
+
+class InitMethodBean implements InitializingBean {
+
+    private String name;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("InitializingBeanTest initializing...");
+        this.name = "chenssy 2 号";
+    }
+
+    public void initMethod(){
+        System.out.println("InitializingBeanTest initMethod22222...");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
